8e8b1bd03d28bbe97a3320ad060aeb8f3af99e39
yj <eric305754@gmail.com>
2024-11-20 19:26:47 +0900

    added superpixel_no_mask function

--- superpixel.py
+++ superpixel.py
@@ -29,3 +29,20 @@ def superpixel(img_resized: np.ndarray, mask_dict: dict) -> np.ndarray:
     done_img = result_img.clip(0, 1)  
 
     return done_img
+
+def superpixel_no_mask(img_resized: np.ndarray) -> np.ndarray:
+
+    img_lab = color.rgb2lab(img_resized)
+    h, w, d = img_lab.shape
+    img_flat = img_lab.reshape(-1).tolist()
+
+    clusters = 100  
+    compactness = 100  
+
+    result = np.array(fslic.fslic(img_flat, w, h, d, clusters, compactness, 10, 1, 1))
+    result = result.reshape(h, w, d)
+    result_rgb = color.lab2rgb(result)
+
+    done_img = result_rgb.clip(0, 1)
+
+    return done_img
